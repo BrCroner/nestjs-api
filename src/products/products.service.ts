1320c7e3cb6e1bd7b7e14dad34c0a3fd72b3ea6d
Bruno Croner <bruno.croner@outlook.com>
2025-02-09 19:13:50 -0300

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ProductSlugAlreadyExistsError } from './erros';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.prismaService.product.findFirst({
+      where: { slug: createProductDto.slug },
+    });
+    if (product) {
+      throw new ProductSlugAlreadyExistsError(createProductDto.slug);
+    }
+    return this.prismaService.product.create({ data: createProductDto });
+  }
+
+  findAll() {
+    return this.prismaService.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.product.findFirst({ where: { id } });
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.prismaService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.product.delete({ where: { id } });
+  }
+}
